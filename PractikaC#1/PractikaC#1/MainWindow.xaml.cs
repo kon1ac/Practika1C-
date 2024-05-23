77a062839efc26a525a66be18de6c30dfac085fa
kon1ac <144265321+kon1ac@users.noreply.github.com>
2024-05-23 10:23:07 +0300

    Add files via upload

---
+++ PractikaC#1/PractikaC#1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using PractikaC_1.Laba1DataSetTableAdapters;
+
+namespace PractikaC_1
+{
+    public partial class MainWindow : Window
+    {
+        CustomersTableAdapter customersTableAdapter = new CustomersTableAdapter();
+        ProductsTableAdapter productsTableAdapter = new ProductsTableAdapter();
+        OrdersTableAdapter ordersTableAdapter = new OrdersTableAdapter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CustomerDataGrid.ItemsSource = customersTableAdapter.GetData();
+            ProductsDataGrid.ItemsSource = productsTableAdapter.GetData();
+            OrdersDataGrid.ItemsSource = ordersTableAdapter.GetData();
+        }
+    }
+}
